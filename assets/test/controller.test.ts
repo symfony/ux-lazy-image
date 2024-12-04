53f1ac328385aa087eff2d4bdb1b06496ea626af
Florian Cellier <florian.cellier@sensiolabs.com>
2024-12-04 11:55:07 +0100

    chore: Apply natural import order for JS to synchronise the code base with Biome and its configuration

--- assets/test/controller.test.ts
+++ assets/test/controller.test.ts
@@ -8,8 +8,8 @@
  */
 
 import { Application, Controller } from '@hotwired/stimulus';
-import { getByTestId, waitFor } from '@testing-library/dom';
 import { clearDOM, mountDOM } from '@symfony/stimulus-testing';
+import { getByTestId, waitFor } from '@testing-library/dom';
 import LazyImageController from '../src/controller';
 
 // Controller used to check the actual controller was properly booted
